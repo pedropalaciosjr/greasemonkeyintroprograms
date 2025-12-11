b5af4f3828a6d3d76543c927ead97be5d0348ee6
Pedro Palacios <109385020+pedropalaciosjr@users.noreply.github.com>
2025-12-11 12:48:13 -0600

    Fundraiser exercise

---
+++ ConcessionStand.java
@@ -0,0 +1,40 @@
+/**
+  Fundraiser Exercise:
+    Topics Covered: Data Types, Variables, User Input, and Math Operations
+
+    Description:
+    You're running the team's biweekly concession stand fundraiser to raise money for competitions, t-shirts, food, etc. 
+    Write a program that asks the customer (user) for their order (only one type), and tells them the total cost of their items.
+    These are the items your team is selling with prices: Hot Dog ($2), Water ($2), and Gatorade ($3.50). 
+    These prices should be stored in your program.
+    If the customer enters an unexpected value, your program should notify them and ask for another item until they respond with "Nevermind". 
+    Terminate after a successful transaction.
+
+    Output examples:
+    ----------------
+    EXAMPLE 1:
+    What would you like to buy?
+    Gatorade
+    How many would you like?
+    2
+    That will be $7!
+    ----------------
+    EXAMPLE 2: 
+    What would you like to buy?
+    Potato Chips
+    We're unfortunately not selling Potato Chips.
+    What would you like to buy?
+    Water
+    How many would you like?
+    1
+    That will be $2!
+    ----------------
+**/
+
+import java.util.Scanner;
+
+public class ConcessionStand {
+    public static void main(String[] args) {
+        // Enter your code here
+    }
+}
